ebf62b3f1c4711b7033ae08d8c2c6998f6c23ce1
MerdanovMekan <83022938+Mekan131998@users.noreply.github.com>
2023-05-01 21:28:41 +0200

    Create week_1_hepde_1.py

---
+++ week_1_hepde_1.py
@@ -0,0 +1,35 @@
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+****Sum of Digits / Digital Root****
+
+Given `n`, take the sum of the digits of `n`. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+## **Examples**
+
+```
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+```
+
+*TKM:* 
+
+********Sifrleriň jemi:******** 
+
+n san berilen, n sanyň sifrleriniň jemini tapmaly. Eger şol jemiň bahasy ýeke sifrden köp bolsa onda only ýeke sifre gelýänçä gaýtalamaly. Netijede otrisatel bolmadyk bitin san alynmaly 
+
+**Mysal üçin,** 
+
+```
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+```
+
+- Solution/ Çözülişi:
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""'
+def digit_sum(n):
+    while n >= 10:
+        n = sum(int(digit) for digit in str(n))
+    return n
